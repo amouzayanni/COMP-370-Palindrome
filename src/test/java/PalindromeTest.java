09c40052264a79605228331b439ffe4ceb0b1ca1
amouzayanni <mouzayannianthony@gmail.com>
2022-05-03 09:23:26 -0500

    laying the groundwork

---
+++ src/test/java/PalindromeTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PalindromeTest {
+    private Palindrome palindrome = new Palindrome();
+    private String str = null;
+    @Test
+    public void testPalindrome() throws Exception{
+        str = "tacocat";
+        assertTrue(Palindrome.isPalindrome(str));
+    }
+}
